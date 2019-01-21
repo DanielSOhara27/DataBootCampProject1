dcbbc07f097aff9ee6dc098d18aeb1b6aa5770bb
DLaPlace <danielsohara@gmail.com>
2019-01-20 21:19:46 -0600

    Preparing to start Amazon Kindle exploration

---
+++ Resources/scripts/amzkindle/amz_kindle_controller.py
@@ -0,0 +1,4 @@
+from os import path
+
+# This script goes over the Amazon's raw 200K reviews from Kaggle - datasets not included in base code
+filepath = path.join("..", "..", "datasets", "unzipped", "kindle-reviews", "kindle_reviews.csv")
